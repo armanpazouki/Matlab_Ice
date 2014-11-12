31bf6affb1799f94c08f8257ec7e52bb20031daf
Arman Pazouki <apazouki@gmail.com>
2014-11-12 15:17:40 -0600

    changes

--- ShipMotion.m
+++ ShipMotion.m
@@ -1,26 +1,19 @@
 clear all
-% close all
+close all
 clc
-vMax = .02; %to normalize time
-L = .8e-3;      %to normalize time
-myData = [];
 fileName = ' ';
-numCompPerRow = 18;%13;
-numParticles = 1;
-% for i = [60 70 61 62 63 64 71 72 73 74]
-for i = [371:372]
-%     figure
-    fileName = strcat('data/dataRigidCenterVsTimeAndDistance_1_', int2str(i) , '.txt');
+for i = [3:3]
+    fileName = strcat('data/forceData00', int2str(i) , '.txt');
    
     delimiterIn = ',';
     headerlinesIn = 1;
     myCellData = importdata(fileName,delimiterIn,headerlinesIn);
     header = char(myCellData.textdata)
     A = myCellData.data;
-    
+    index = find(A(:, 1) < 3.6);
     str1 = strcat('run: ', num2str(i));
     figure
-    plot(A(:, 2), A(:, 8),'.');
+    plot(A(index, 1), A(index, 5),'.');
     title(str1);
     % -- end of title and such
 end
